c1ef6ba06c52ac6f19ddf912c30569ad65e5a787
John Ferreira <johnferreira@gmail.com>
2026-02-20 09:49:42 +0100

    [2026-02-20 09:39][B0][FIX][db] Widen catalog_recipes varchar columns to text to fix 500 on save

---
+++ supabase/migrations/20260220095000_widen_recipe_varchar_columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.catalog_recipes ALTER COLUMN meal_slot TYPE text;
+ALTER TABLE public.catalog_recipes ALTER COLUMN category TYPE text;
+ALTER TABLE public.catalog_recipes ALTER COLUMN cuisine TYPE text;
+ALTER TABLE public.catalog_recipes ALTER COLUMN difficulty TYPE text;
+ALTER TABLE public.catalog_recipes ALTER COLUMN language TYPE text;
